610a3284ee5f49c5c8499b0303df9664538a0d63
highjump <lixiang901112@163.com>
2017-10-21 16:26:06 +0800

    Added UI for profile page

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -1,4 +1,7 @@
 // pages/profile/profile.js
+
+const app = getApp();
+
 Page({
 
   /**
@@ -12,7 +15,30 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
   },
 
   /**
