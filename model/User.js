33d5c52b4bc14b63050ab011a0f0ca0bdaf454e4
highjump <lixiang901112@163.com>
2018-04-17 00:26:20 +0800

    Added saveTreat api

--- model/User.js
+++ model/User.js
@@ -2,6 +2,18 @@ class User {
 
   static Roles = ['普通会员', '设备管理员','维修工程师', '平台管理员', '系统管理员'];
 
+  // 角色
+  // 0：普通会员
+  static ROLE_NORMAL = 0;
+  // 1：设备管理员
+  static ROLE_DEVICE_ADMIN = 1;
+  // 2：维修工程师
+  static ROLE_MAINTAIN_ENGINEER = 2;
+  // 3：平台管理员
+  static ROLE_PLATFORM_ADMIN = 3;
+  // 4：系统管理员
+  static ROLE_SYSTEM_ADMIN = 4;
+
   constructor (userInfo) {
     // 初始化
     this.nickName = userInfo.nickName;
@@ -14,10 +26,7 @@ class User {
 
     // 数据
 
-    //
-    // 角色，0：普通会员，1：设备管理员，2：维修工程师，3：平台管理员，4：系统管理员
-    //
-    this.role = 0; // 普通会员
+    this.role = User.ROLE_NORMAL; // 普通会员
   }
 
   getLocationFormatted() {
@@ -25,11 +34,10 @@ class User {
   }
   
   setRoleFromString(strRole) {
-    if (strRole == '普通会员') {
-      this.role = 0;
-    }
-    else if (strRole == '系统管理员') {
-      this.role = 4;
+    for (var i = 0; i < User.Roles.length; i++) {
+      if (strRole == User.Roles[i]) {
+        this.role = i;
+      }
     }
   }
 }
